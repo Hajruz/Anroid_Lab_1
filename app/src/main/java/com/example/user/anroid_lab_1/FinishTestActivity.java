f4962c3cc540a9057cf1cc7d905a5e01af5f8b86
Ruzalin Khaibriev <ruzalin4ik@mail.ru>
2017-09-20 02:18:37 +0300

    Android_Lab_1

--- app/src/main/java/com/example/user/anroid_lab_1/FinishTestActivity.java
+++ app/src/main/java/com/example/user/anroid_lab_1/FinishTestActivity.java
@@ -1,6 +1,5 @@
 package com.example.user.anroid_lab_1;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
